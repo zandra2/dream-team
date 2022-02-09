7786b1d1a5ea420480b9471e52e39781faa7679f
Nourhan Alenany <alenany.n21@gmail.com>
2022-02-10 00:17:41 +0200

    App js & css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+// styles
 import "./App.css";
 
+// pages & components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Create from "./pages/create/Create";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import OnlineUsers from "./components/OnlineUsers";
+
 function App() {
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* only render when authentication is ready */}
+      {authIsReady && (
+        <BrowserRouter>
+          {/* appears on the right when user logged in */}
+          {user && <Sidebar />}
+          <div className="container">
+            {/* appears in the center */}
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                {/* if user not authenticated redirect to login page */}
+                {!user && <Redirect to="/login" />}
+                {/* if user is logged in then they can see Dashboard */}
+                {user && <Dashboard />}
+              </Route>
+              <Route path="/create">
+                {!user && <Redirect to="/login" />}
+                {user && <Create />}
+              </Route>
+              <Route path="/projects/:id">
+                {!user && <Redirect to="/login" />}
+                {user && <Project />}
+              </Route>
+              <Route path="/login">
+                {user && <Redirect to="/" />}
+                {!user && <Login />}
+              </Route>
+              <Route path="/signup">
+                {user && user.displayName && <Redirect to="/" />}
+                {!user && <Signup />}
+              </Route>
+            </Switch>
+          </div>
+          {/* appears on the left when user logged in */}
+          {user && <OnlineUsers />}
+        </BrowserRouter>
+      )}
     </div>
   );
 }
