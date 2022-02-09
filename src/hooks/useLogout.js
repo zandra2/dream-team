3f3fac4394d9fc2a79cdb5c5dd7efb8e1560ebf4
Nourhan Alenany <alenany.n21@gmail.com>
2022-02-10 00:00:18 +0200

    logout hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { projectAuth, projectFirestore } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch, user } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // update online status when user logs out
+      const { uid } = user;
+      // const { uid } = projectAuth.currentUser;
+      await projectFirestore
+        .collection("users")
+        .doc(uid)
+        .update({ online: false });
+
+      // sign the user out after we update the online status
+      await projectAuth.signOut();
+
+      // dispatch logout action
+      dispatch({ type: "LOGOUT" });
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCancelled) {
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { logout, error, isPending };
+};
