000e385c66834915cc82cd1c5ed46b93757f36ba
Nourhan Alenany <alenany.n21@gmail.com>
2022-02-09 23:55:51 +0200

    firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/storage";
+
+// Zandra's or Nourhan's API Key
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// init firebase - application
+firebase.initializeApp(firebaseConfig);
+
+// init services - firetstore, authentication & storage
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp, projectStorage };
