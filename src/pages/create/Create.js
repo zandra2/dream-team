0118784526dad8ea9f7e4b7af2508fd29e274e21
Zandra Nguyen <zandra.eng@gmail.com>
2022-02-09 17:50:30 -0800

    created create folder, create.js and create.css. This allow user to create a new project, set project details, due date, pick a category and assign user.

---
+++ src/pages/create/Create.js
@@ -0,0 +1,148 @@
+// Router5 vs Router6: useHistroy = useNavigate
+import { useState, useEffect } from "react";
+import { useCollection } from "../../hooks/useCollection";
+import Select from "react-select";
+import { timestamp } from "../../firebase/config";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
+import { useNavigate } from "react-router";
+import "./Create.css";
+
+// form field values
+const categories = [
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
+
+export default function Create() {
+  const { documents } = useCollection("users");
+  const [users, setUsers] = useState([]);
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+  const { addDocument, response } = useFirestore("projects");
+
+  // form field values
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
+  const [assignedUsers, setAssignedUsers] = useState([]); //isMulti will update this array
+  const [formError, setFormError] = useState(null);
+
+  // create user values for react-select
+  useEffect(() => {
+    if (documents) {
+      setUsers(
+        documents.map((user) => {
+          return { value: { ...user, id: user.id }, label: user.displayName };
+        })
+      );
+    }
+  }, [documents]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setFormError(null); //set form null after every submit
+
+    //after submit, preform a check for each form
+    if (!category) {
+      setFormError("Please select a project category.");
+      return;
+    }
+
+    //an empty arry is a value, therefore we need to check the length logic instead
+    if (assignedUsers.length < 1) {
+      setFormError("Please assign the project to at least 1 user");
+      return;
+    }
+
+    //want to create a different array object; an array w/only 3 properties
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      };
+    });
+
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.uid,
+    };
+
+    const project = {
+      name,
+      details,
+      assignedUsersList,
+      createdBy,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+    };
+
+    console.log(project);
+
+    // if there is no error on the response, then re-direct user to end of dashboard(home) page using history object
+    await addDocument(project);
+    if (!response.error) {
+      navigate("/"); //router5 = history.push("/")
+    }
+  };
+
+  return (
+    <div className="create-form">
+      <h2 className="page-title">Create a new Project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project Details:</span>
+          <textarea
+            required
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select
+            onChange={(option) => setAssignedUsers(option)}
+            options={users}
+            // isMulti allows selecting mulitple users
+            isMulti
+          />
+        </label>
+
+        <button className="btn">Add Project</button>
+
+        {formError && <p className="error">{formError}</p>}
+      </form>
+    </div>
+  );
+}
